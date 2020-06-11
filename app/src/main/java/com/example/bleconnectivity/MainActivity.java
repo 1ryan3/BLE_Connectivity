1e928a9ab4cb7e214ce6103d3ba1e20ba230eb4f
Ryan Wood <2ryanwood6@gmail.com>
2020-06-11 15:22:30 -0700

    Initial commit -- enough work done to justify version control...

---
+++ app/src/main/java/com/example/bleconnectivity/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.bleconnectivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(this, "The permission to get BLE location data is required", Toast.LENGTH_SHORT).show();
+            }else{
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }else{
+            Toast.makeText(this, "Location permissions already granted", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+        Button btnConnect = (Button) findViewById(R.id.ConnectButton);
+        btnConnect.setOnClickListener(handler);
+    }
+    private View.OnClickListener handler = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Intent intentMain = new Intent(MainActivity.this, DeviceConnectActivity.class);
+            MainActivity.this.startActivity(intentMain);
+        }
+    };
+}
