9b6198e57d68c398db5bf34ad014511122c7da6a
Ryan Wood <2ryanwood6@gmail.com>
2020-06-25 23:07:27 -0700

    Using onValueChange, colors change 'instantly'. Added color visualizer

--- app/src/main/java/com/example/bleconnectivity/editGroup.java
+++ app/src/main/java/com/example/bleconnectivity/editGroup.java
@@ -7,9 +7,13 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.IBinder;
-
+import android.view.View;
+import android.widget.TextView;
 
 
 import com.google.android.material.slider.Slider;
@@ -19,6 +23,10 @@ public class editGroup extends AppCompatActivity {
     private Slider red;
     private Slider blue;
     private Slider green;
+    private TextView rDisp;
+    private TextView gDisp;
+    private TextView bDisp;
+    private View colorSquare;
     private float redValue = 255;
     private float greenValue = 255;
     private float blueValue = 255;
@@ -31,57 +39,54 @@ public class editGroup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_group);
 
+        colorSquare = findViewById(R.id.colorSquare);
+
         red = findViewById(R.id.redSlider);
         red.setValue(redValue);
-        red.addOnSliderTouchListener(redHandler);
+        red.addOnChangeListener(redHandler);
 
         green = findViewById(R.id.greenSlider);
         green.setValue(greenValue);
-        green.addOnSliderTouchListener(blueHandler);
+        green.addOnChangeListener(greenHandler);
+
+        rDisp = findViewById(R.id.redDispVal);
+        bDisp = findViewById(R.id.blueDispVal);
+        gDisp = findViewById(R.id.greenDispVal);
 
         blue = findViewById(R.id.blueSlider);
         blue.setValue(blueValue);
-        blue.addOnSliderTouchListener(greenHandler);
+        blue.addOnChangeListener(blueHandler);
         bindService();
         groupNum = getIntent().getStringExtra("groupNumber");
     }
 
-    private Slider.OnSliderTouchListener redHandler = new Slider.OnSliderTouchListener() {
+    private Slider.OnChangeListener redHandler = new Slider.OnChangeListener() {
         @Override
-        public void onStartTrackingTouch(@NonNull Slider slider) {
-
-        }
-
-        @Override
-        public void onStopTrackingTouch(@NonNull Slider slider) {
+        public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
             redValue = slider.getValue();
+            rDisp.setText(Integer.toHexString((int)redValue).toUpperCase());
             setColor();
+            updatePreview();
         }
     };
 
-    private Slider.OnSliderTouchListener greenHandler = new Slider.OnSliderTouchListener() {
+    private Slider.OnChangeListener greenHandler = new Slider.OnChangeListener() {
         @Override
-        public void onStartTrackingTouch(@NonNull Slider slider) {
-
-        }
-
-        @Override
-        public void onStopTrackingTouch(@NonNull Slider slider) {
+        public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
             greenValue = slider.getValue();
+            gDisp.setText(Integer.toHexString((int)greenValue).toUpperCase());
             setColor();
+            updatePreview();
         }
     };
 
-    private Slider.OnSliderTouchListener blueHandler = new Slider.OnSliderTouchListener() {
+    private Slider.OnChangeListener blueHandler = new Slider.OnChangeListener() {
         @Override
-        public void onStartTrackingTouch(@NonNull Slider slider) {
-
-        }
-
-        @Override
-        public void onStopTrackingTouch(@NonNull Slider slider) {
+        public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
             blueValue = slider.getValue();
+            bDisp.setText(Integer.toHexString((int)blueValue).toUpperCase());
             setColor();
+            updatePreview();
         }
     };
 
@@ -91,10 +96,15 @@ public class editGroup extends AppCompatActivity {
 
         byte[] data = new byte[4];
         data[0] = (byte) redValue;
-        data[1] = (byte) blueValue;
-        data[2] = (byte) greenValue;
+        data[1] = (byte) greenValue;
+        data[2] = (byte) blueValue;
         data[3] = (byte) temp;
         mService.colorSet(data);
+    }
+
+    private void updatePreview() {
+        int color = (0xff) << 24 | ((int)redValue & 0xff) << 16 | ((int)greenValue & 0xff) << 8 | ((int)blueValue & 0xff);
+        colorSquare.setBackgroundColor(color);
     }
 
     private void bindService(){
